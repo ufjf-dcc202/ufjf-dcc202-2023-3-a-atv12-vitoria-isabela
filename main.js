eb029a90a93f2c189134dcddfda511b68be0fa3c
vitoria-isabela <VitoriaIsabela.deOliveira@thomsonreuters.com>
2023-12-19 20:13:57 -0300

    add leFormulario function

--- main.js
+++ main.js
@@ -5,6 +5,29 @@ document.entrada.addEventListener('submit', leFormulario)
 const olJoao = document.querySelector("#joao")
 const olMaria = document.querySelector("#maria")
 
+atualizaTela()
+
+function leFormulario(event) {
+    event.preventDefault()
+    
+    const fruta = document.entrada.fruta.value
+    const quantidade = document.entrada.quantidade.valueAsNumber
+    const origem = document.entrada.origem.value
+    const destino = document.entrada.destino.value
+    
+    console.log(`solicitado: ${origem} doa ${quantidade} ${fruta} para ${destino}`)
+
+    transacaoNoEstoque(origem, destino, fruta, quantidade)
+    atualizaTela()
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('botaoLimparLista').addEventListener('click', () => {
+        limpaEstoque()
+        atualizaTela()
+    })
+})
+
 function atualizaTela() {
     const estoque = getEstoque()
     
